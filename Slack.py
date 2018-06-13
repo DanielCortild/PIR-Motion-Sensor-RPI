fb0dc56ab5b4dc408f75733343bf7a2ddf182958
DanielCortild <daniel@cortild.dk>
2018-06-13 16:46:19 +0200

    Correction

--- Slack.py
+++ Slack.py
@@ -26,15 +26,19 @@ mouv = 0
 while True:
     try:
         if grovepi.digitalRead(pir_sensor):
-            mouv+=1
-		else:
-			mouv-=1
-			if mouv<0: mouv=0
-		if mouv == 10:
+            mouv = mouv+1
+        else:
+            if mouv > 0:
+				mouv = mouv-1
+
+        if mouv == 10:
+			print("Mouvmement")
 			send()
 			mouv=0
 			time.sleep(30)
-        time.sleep(1)
+
+        time.sleep(.2)
+
 
     except IOError:
         print "Error"
